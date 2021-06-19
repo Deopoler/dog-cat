0199a38e50e182d353f10fcc5a8611a1dd5be293
Deopoler <seonjin6804@gmail.com>
2021-06-19 17:22:34 +0900

    add debug print

--- app/main.py
+++ app/main.py
@@ -16,16 +16,20 @@ def predict():
     if (request.method == "POST"):
         file = request.files.get('file')
         if file is None or file.filename == "":
+            print("error: no file")
             return jsonify({'error': 'no file'})
 
         if not allowed_file(file.filename):
+            print("error: format not supported")
             return jsonify({'error': 'format not supported'})
 
         try:
             img_bytes = file.read()
             tensor = preprogress(img_bytes)
             pred = get_predict(tensor)
+            print("success")
             return jsonify({'cat': pred[0][0], 'dog': pred[0][1]})
 
         except:
+            print("error: error during prediction")
             return jsonify({'error': 'error during prediction'})
